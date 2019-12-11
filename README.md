79dd58d6c799dd360dd125efd6f3ca9cca452b70
Robert Heitzmann <Robert.Heitzmann@me.com>
2019-12-11 10:35:53 +0100

    small update to Readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# advent_of_code_2018
+Code (solutions) to Advent of Code 2018 challenges.
+By kannix68
--- day02/day02b.clj
+++ day02/day02b.clj
@@ -1,19 +1,22 @@
 #!/usr/bin/env clojure
 
-; Advent of code 2018, AoC day 2 part 2.
+; Advent of code 2018, AoC day 2 part 1.
 ; This solution (clojure 1.8) by kannix68, @ 2018-12-02.
 ; Save your specific input as "day##.in".
 ;
-; tips:
-; * [diff - clojure.data](https://clojuredocs.org/clojure.data/diff)
-; * [combinatorics - Enumerate k-combinations in Clojure (P26 from 99 problems)](<https://codereview.stackexchange.com/questions/8930/enumerate-k-combinations-in-clojure-p26-from-99-problems>)
-;     * [H-99: Ninety-Nine Haskell Problems](https://wiki.haskell.org/H-99:_Ninety-Nine_Haskell_Problems)
-;
 
 (require '[clojure.string :as str]) ; clj str ops
-(require '[clojure.data :as dat])
-(require '[clojure.set :as set])
-;(require '[clojure.math.combinatorics :as combi]) ; non-core dependency
+
+;** test data (as a var(iable))
+(def teststr1 "abcdef
+bababc
+abbcde
+abcccd
+aabcdd
+abcdee
+ababab")
+
+;** "lib" functions
 
 (defn assert-msg [condit msg]
   "check condition, fail with message on false, print message otherwise"
@@ -23,11 +26,6 @@
 )
 
 ;** problem solution
-(defn explode-str [str]
-  "explode a (singleline) string into a listof characters."
-  (str/split str #"")
-)
-
 (defn explode-str-lol [str]
   "explode a (multiline) string into list-of-lists,
    containing single characters grouped by line."
@@ -48,67 +46,28 @@
   )
 )
 
-(defn combinations [s k]
-  "Given a set S and a no. of items K,
-  returns all possible combinations of K items that can be taken from set S."
-  (cond
-    (> k (count s)) nil    ;not enough items in sequence to form a valid combination
-    (= k (count s)) [s]    ;only one combination available: all items 
-    (= 1 k) (map vector s) ;every item (on its own) is a valid combination
-    :else (reduce concat (map-indexed 
-            (fn [i x] (map #(cons x %) (combinations (drop (inc i) s) (dec k)))) 
-            s))))
-
-;(defn str-dist [s1 s2]
-(defn lst-dist [l1 l2]
-  "how many items in lists are different?, returniing int"
-  (count (filter identity (first (dat/diff l1 l2)))) ; filter identity removes nils
-)
-
-;** test data (as a var(iable))
-(def teststr1 "abcde
-fghij
-klmno
-pqrst
-fguij
-axcye
-wvxyz")
-
 ;** MAIN
 
 ; solve
 (defn solve [input]
   (let [
+    ;deb 12 ; max take slice items
     lol (explode-str-lol input)
-    ;lstdst (lst-dist (explode-str "ab") (explode-str "ac"))
-    ;lstdst2 (lst-dist (explode-str "abc") (explode-str "cba"))
-    ;lstdst3 (lst-dist (explode-str "abcde") (explode-str "axcye"))
-    ;lstdst4 (lst-dist (explode-str "fghij") (explode-str "fguij"))
-    ;combis (combi/combinations lol 2)
-    combis (combinations lol 2)
-    fltrdlst (apply concat (filter (fn[lst] (= 1 (apply lst-dist lst))) combis)) ; apply concat unwrap one level
-    intrsect (set/intersection (set(first fltrdlst)) (set(last fltrdlst)))
-    ;dsts (map lst-dist combis)
-    result (str/join (filter #(contains? intrsect %) (first fltrdlst))) ; in original order!
+    count2s (count-occur-in lol 2)
+    count3s (count-occur-in lol 3)
+    result 0
     ]
     ;(println "input\n" input)
     (println "list-count" (count lol)) ;"list-of-lists" lol)
-    ;(println "lstdst:" lstdst)
-    ;(println "lstdst2:" lstdst2)
-    ;(println "lstdst3:" lstdst3)
-    ;(println "lstdst4:" lstdst4)
-    ;(println "combis:" combis)
-    ;(println "fltrdlst:" fltrdlst)
-    ;(println "intrsect:" intrsect)
-    (println "result:" result)
-    ;(println "dsts:" dsts)
-    result
+    (println "count-occur-in lol 2:" count2s)
+    (println "count-occur-in lol 3:" count3s)
+    (* count2s count3s)
   )
 )
 
 ; solve/assert test-data
 (let [
-  expected "fgij"
+  expected 12
   result (solve teststr1)
   ]
   (assert-msg (= result expected) (str "test-result " result " should be " expected))
